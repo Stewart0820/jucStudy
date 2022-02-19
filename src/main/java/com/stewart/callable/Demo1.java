43d1ee3725fd6bcb7dd95c3987d8e6b0148b75be
Stewart <sukura13123133927@qq.com>
2022-02-19 10:11:01 +0800

    使用Callable接口创建线程，需要通过FutureTask未来任务 //简便的写法  FutureTask futureTask2 = new FutureTask<>(()->{      return 1024; });

--- src/main/java/com/stewart/callable/Demo1.java
+++ src/main/java/com/stewart/callable/Demo1.java
@@ -1,9 +1,55 @@
 package com.stewart.callable;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @author Stewart
  * @create 2022/2/19
- * @Description 比较
+ * @Description 比较Runable和Callable接口
  */
+class MyThread1 implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
+class MyThread2 implements Callable{
+
+    @Override
+    public Object call() throws Exception {
+        return 200;
+    }
+}
 public class Demo1 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //使用Runable的方式
+        new Thread(new MyThread1(),"A").start();
+
+        //Callable的方式
+        //需要通过FutureTask
+        FutureTask futureTask1 = new FutureTask<>(new MyThread2());
+
+        //简写
+        FutureTask futureTask2 = new FutureTask<>(()->{
+            System.out.println(Thread.currentThread().getName()+"come in callable");
+            return 1024;
+        });
+
+        //创建一个线程
+        new Thread(futureTask2,"lucy").start();
+        new Thread(futureTask1,"mary").start();
+
+
+        while (!futureTask2.isDone()){
+            System.out.println("wait.....");
+        }
+        System.out.println(futureTask2.get());
+        System.out.println(futureTask2.get());
+        System.out.println(Thread.currentThread().getName()+"结束");
+
+        //FutureTask 未来任务
+    }
 }
